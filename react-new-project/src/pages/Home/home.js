90c22cc538644b9c9922675fbf997108641bf116
tnav97 <Naveen.t@qbrainx.com>
2023-04-21 17:06:22 +0530

    Update home.js

--- react-new-project/src/pages/Home/home.js
+++ react-new-project/src/pages/Home/home.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
 
 function Home() {
     return (<>
@@ -8,11 +13,26 @@ function Home() {
     <p>Home</p>
     
     <button >Place order</button>
-    <Stack spacing={2} direction="row">
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-    </Stack>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image='https://images.unsplash.com/photo-1522770179533-24471fcdba45'
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
     </div>
     </>
         
